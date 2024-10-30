8e847c3f2854c4a40928b65c15a5bf028cfb76a9
eve-wils <evelyn.wilson@okstate.edu>
2024-10-29 19:12:18 -0500

    Finish exercise 2

--- Lab 5/static/parse_data.js
+++ Lab 5/static/parse_data.js
@@ -9,28 +9,47 @@ if (!!window.EventSource) {
       // finish the code to handle the bumper status
         if (bumper=="0")
           {
-            document.getElementById("but1").value = "OFF";
+            document.getElementById("bumpertoggle").value = "OFF";
           }
         if (bumper=="1")
         {
-          document.getElementById("but1").value = "Right";
+          document.getElementById("bumpertoggle").value = "Right";
         }
+        if (bumper == "2")
+          {
+            document.getElementById("bumpertoggle").value = "Center";
+          }
+        if (bumper == "4")
+        {
+          document.getElementById("bumpertoggle").value = "Left";
+        }
+  
         
          
         
         // finish the code to handle the wheel drop status 
       if (drop=="0")
         {
-          document.getElementById("the id of button where you need to display the sensor status").value = "OFF";
-
+          document.getElementById("wheeldroptoggle").value = "OFF";
+        }
+      if (drop=="1")
+        {
+          document.getElementById("wheeldroptoggle").value = "Right";
+        }
+      if (drop=="2")
+        {
+          document.getElementById("wheeldroptoggle").value = "Left";
         }
       
 
       // finish the code to handle cliff status 
       if (cliff=="0")
         {
-          document.getElementById("the id of button where you need to display the sensor status").value = "OFF";
+          document.getElementById("clifftoggle").value = "OFF";
         }
+      else {
+          document.getElementById("clifftoggle").value = "ON";
+      }
       
 
 
