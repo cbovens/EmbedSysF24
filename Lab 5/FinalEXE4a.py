19a6d4128ec5c9ee363877dc62f9adf3688f3f5d
cbovens <collinbovenschen03@gmail.com>
2024-11-19 22:00:06 -0600

    Lab 5 4b done?

--- Lab 5/FinalEXE4a.py
+++ Lab 5/FinalEXE4a.py
@@ -1,9 +1,9 @@
-#This is a basic script to host a webpage at the IP specified
+# This is a basic script to host a webpage at the IP specified
 # By the 'IP_Address' variable
 
 # Import library to create webserver to host webpage
 from flask import Flask, render_template
-from flask import Flask, render_template, Response,redirect,request, url_for
+from flask import Flask, render_template, Response, redirect, request, url_for
 import itertools
 # import time
 from camera_pi import Camera
@@ -13,25 +13,24 @@ import socket
 app = Flask(__name__)
 
 
-server_address_1 =  ('127.0.0.2', 8001)
+server_address_1 = ('127.0.0.2', 8001)
 sock_1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock_1.bind(server_address_1)
 
 
-
-server_address =  ('127.0.0.1', 8000)
+server_address = ('127.0.0.1', 8000)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(server_address)
 sock.listen(5)
 connection, address = sock.accept()
 
 
-#Find the IP Address of your device
-#Use the 'ifconfig' terminal command, the address should be in
-#the format  "XX.XXX.XXX.XXX"
-IP_Address = '10.227.96.8' # CHANGE EVERY TIME, Raspberry Pi IP
+# Find the IP Address of your device
+# Use the 'ifconfig' terminal command, the address should be in
+# the format  "XX.XXX.XXX.XXX"
+IP_Address = '10.227.96.8'  # CHANGE EVERY TIME, Raspberry Pi IP
 PORT = 8080
-#Connect the *.html page to the server and run as the default page
+# Connect the *.html page to the server and run as the default page
 
 
 @app.route('/')
@@ -39,7 +38,7 @@ def index():
     if request.headers.get('accept') == 'text/event-stream':
         def events():
             for i, c in enumerate(itertools.cycle('\|/-')):
-            # info = connection.recv(6).decode("utf-8")   
+                # info = connection.recv(6).decode("utf-8")
                 yield "data: %s\n\n" % ('b0c0d0')
         return Response(events(), content_type='text/event-stream')
     return render_template('FinalEXE3.html')
@@ -50,52 +49,56 @@ def gen(camera):
     frame = ''
     while True:
         # receive image to the client: frame = .....
-        frame,_ = sock_1.recvfrom(max_len)
+        frame, _ = sock_1.recvfrom(max_len)
         yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/UpFunction')
 def UpFunction():
     print('In UpFunction')
     cmd = 'u'
-    connection.send(cmd.encode('utf-8'))  
+    connection.send(cmd.encode('utf-8'))
     return "None"
+
 
 @app.route('/DownFunction')
 def DownFunction():
     print('In DownFunction')
     cmd = 'd'
-    connection.send(cmd.encode('utf-8'))  
+    connection.send(cmd.encode('utf-8'))
     return "Nothing"
+
 
 @app.route('/LeftFunction')
 def LeftFunction():
     print('In LeftFunction')
     cmd = 'l'
-    connection.send(cmd.encode('utf-8'))  
+    connection.send(cmd.encode('utf-8'))
     return "Nothing"
+
 
 @app.route('/RightFunction')
 def RightFunction():
     print('In RightFunction')
     cmd = 'r'
-    connection.send(cmd.encode('utf-8'))  
+    connection.send(cmd.encode('utf-8'))
     return "Nothing"
+
 
 @app.route('/StopFunction')
 def StopFunction():
     print('In StopFunction')
     cmd = 's'
-    connection.send(cmd.encode('utf-8'))  
+    connection.send(cmd.encode('utf-8'))
     return "Nothing"
 
-#Start the server
+
+# Start the server
 if __name__ == "__main__":
     app.run(debug=True, host=IP_Address, port=PORT, use_reloader=False)
--- Lab 5/FinalEXE4b_CPP.cpp
+++ Lab 5/FinalEXE4b_CPP.cpp
@@ -1,6 +1,5 @@
 // g++ -std=c++11 -o FinalEXE4b_CPP FinalEXE4b_CPP.cpp -lwiringPi -pthread
 
-
 #include <iostream>
 #include <unistd.h>
 #include <wiringSerial.h>
@@ -12,7 +11,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include<cstring>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
@@ -20,13 +19,13 @@
 #include <thread>
 
 using namespace std;
-#define  PORT 8000
-#define  IP "127.0.0.1"
+#define PORT 8000
+#define IP "127.0.0.1"
 
 int sock = 0;
 void movement(int, int);
-FILE * file;
-char checksum(char *packet,int packet_size);
+FILE *file;
+char checksum(char *packet, int packet_size);
 int createSocket();
 int kobuki;
 
@@ -46,102 +45,116 @@ void readData();
 void rotateFunc(bool);
 void linMoveFunc(int);
 
-
-void read_socket(){
+void read_socket()
+{
 	char buffer[100];
-	while(1){
-		read(sock , buffer, 50);
+	while (1)
+	{
+		read(sock, buffer, 50);
 		/*Print the data to the terminal*/
 		cmd = buffer[0];
-		printf("received: %c\n",cmd);
+		printf("received: %c\n", cmd);
 
 		// use cmd to control the robot movement
-		switch (cmd){
-			case 'u': linMoveFunc(1); break;
-			case 'd': linMoveFunc(-1); break;
-			case 'r': rotateFunc(false); break;
-			case 'l': rotateFunc(true); break;
-			case 's': movement(0,0); break;
-			default: break;
+		switch (cmd)
+		{
+		case 'u':
+			linMoveFunc(1);
+			break;
+		case 'd':
+			linMoveFunc(-1);
+			break;
+		case 'r':
+			rotateFunc(false);
+			break;
+		case 'l':
+			rotateFunc(true);
+			break;
+		case 's':
+			movement(0, 0);
+			break;
+		default:
+			break;
 		}
-		//clean the buffer
+		// clean the buffer
 		memset(&buffer, '0', sizeof(buffer));
 	}
-	
 }
 
-int main(){
+int main()
+{
 	setenv("WIRINGPI_GPIOMEM", "1", 1);
 	wiringPiSetup();
 	kobuki = serialOpen("/dev/kobuki", 115200);
 	createSocket();
 	char buffer[10];
-	std::thread  t(read_socket);
+	std::thread t(read_socket);
 
-	while(serialDataAvail(kobuki) != -1)
+	while (serialDataAvail(kobuki) != -1)
 	{
 		// Read the sensor data.
 		readData();
 
 		// Construct an string data like 'b0c0d0', you can use "sprintf" function. You can also define your own data protocal.
 		char inp[7] = "b0c0d0";
-		/*if ((cliff > 0x07) || (bumper  > 0x07) || (drop > 0x07))
+		if ((cliff > 0x07) || (bumper > 0x07) || (drop > 0x07))
 		{
 			bumper = 0;
 			cliff = 0;
 			drop = 0;
-		} */
+		}
 		inp[1] = '0' + bumper;
 		inp[3] = '0' + cliff;
 		inp[5] = '0' + drop;
 
-
 		// Send the sensor data through the socket
-		send(sock, str, sizeof(str), 0);
+		send(sock, inp, sizeof(inp), 0);
 		// Clear the buffer
-		memset(&buffer, '0', sizeof(buffer)); //maybe dont need
-		// You can refer to the code in previous labs. 
+		// memset(&buffer, '0', sizeof(buffer)); // maybe dont need
+		// You can refer to the code in previous labs.
 	}
 	serialClose(kobuki);
-	
-	return(0);
+
+	return (0);
 }
 
-void movement(int sp, int r){
-	//Create the byte stream packet with the following format:
-	unsigned char b_0 = 0xAA; /*Byte 0: Kobuki Header 0*/
-	unsigned char b_1 = 0x55; /*Byte 1: Kobuki Header 1*/
-	unsigned char b_2 = 0x06; /*Byte 2: Length of Payload*/
-	unsigned char b_3 = 0x01; /*Byte 3: Payload Header*/
-	unsigned char b_4 = 0x04; /*Byte 4: Payload Data: Length*/
-	unsigned char b_5 = sp & 0xff;	/*Byte 5: Payload Data: Speed(mm/s)*/
-	unsigned char b_6 = (sp >> 8) & 0xff;	/*Byte 6: Payload Data: Speed(mm/s)*/
-	unsigned char b_7 = r & 0xff;	/*Byte 7: Payload Data: Radius(mm)*/
-	unsigned char b_8 = (r >> 8) & 0xff;		/*Byte 8: Payload Data: Radius(mm)*/
-	unsigned char checksum = 0;		/*Byte 9: Checksum*/
+void movement(int sp, int r)
+{
+	// Create the byte stream packet with the following format:
+	unsigned char b_0 = 0xAA;			  /*Byte 0: Kobuki Header 0*/
+	unsigned char b_1 = 0x55;			  /*Byte 1: Kobuki Header 1*/
+	unsigned char b_2 = 0x06;			  /*Byte 2: Length of Payload*/
+	unsigned char b_3 = 0x01;			  /*Byte 3: Payload Header*/
+	unsigned char b_4 = 0x04;			  /*Byte 4: Payload Data: Length*/
+	unsigned char b_5 = sp & 0xff;		  /*Byte 5: Payload Data: Speed(mm/s)*/
+	unsigned char b_6 = (sp >> 8) & 0xff; /*Byte 6: Payload Data: Speed(mm/s)*/
+	unsigned char b_7 = r & 0xff;		  /*Byte 7: Payload Data: Radius(mm)*/
+	unsigned char b_8 = (r >> 8) & 0xff;  /*Byte 8: Payload Data: Radius(mm)*/
+	unsigned char checksum = 0;			  /*Byte 9: Checksum*/
 	// file = fopen("/dev/kobuki","w");
-	char packet[] = {b_0,b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8};
+	char packet[] = {b_0, b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8};
 	for (unsigned int i = 2; i < 9; i++)
 		checksum ^= packet[i];
-	serialPutchar (kobuki, b_0);
-	serialPutchar (kobuki, b_1);
-	serialPutchar (kobuki, b_2);
-	serialPutchar (kobuki, b_3);
-	serialPutchar (kobuki, b_4);
-	serialPutchar (kobuki, b_5);
-	serialPutchar (kobuki, b_6);
-	serialPutchar (kobuki, b_7);
-	serialPutchar (kobuki, b_8);
-	serialPutchar (kobuki, checksum);
+	serialPutchar(kobuki, b_0);
+	serialPutchar(kobuki, b_1);
+	serialPutchar(kobuki, b_2);
+	serialPutchar(kobuki, b_3);
+	serialPutchar(kobuki, b_4);
+	serialPutchar(kobuki, b_5);
+	serialPutchar(kobuki, b_6);
+	serialPutchar(kobuki, b_7);
+	serialPutchar(kobuki, b_8);
+	serialPutchar(kobuki, checksum);
 	delay(30);
 }
 
-
-int createSocket(){
+int createSocket()
+{
 	struct sockaddr_in address;
 	struct sockaddr_in serv_addr;
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
 		printf("\nSocket creation error \n");
 		return -1;
 	}
@@ -149,62 +162,66 @@ int createSocket(){
 	memset(&serv_addr, '0', sizeof(serv_addr));
 
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port   = htons(PORT);
+	serv_addr.sin_port = htons(PORT);
 
 	/*Use the IP address of the server you are connecting to*/
-	if(inet_pton(AF_INET, IP , &serv_addr.sin_addr) <= 0){
+	if (inet_pton(AF_INET, IP, &serv_addr.sin_addr) <= 0)
+	{
 		printf("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
 
-	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
 		printf("\nConnection Failed \n");
 		return -1;
 	}
 	return 0;
 }
 
-void readData(){
+void readData()
+{
 	unsigned int read;
-	while(true){
-		//If the bytes are a 1 followed by 15, then we are
-		//parsing the basic sensor data packet
-			read = serialGetchar(kobuki);
-			if(read == 1){
-				if(serialGetchar(kobuki) == 15) break;
-			}
-		}
-
-		//Read past the timestamp
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
-		/*Read the bytes containing the bumper, wheel drop,
-			and cliff sensors.*/
-		bumper = serialGetchar(kobuki);
-		drop = serialGetchar(kobuki);
-		cliff = serialGetchar(kobuki);
-
-		/*Read through the bytes between the cliff sensors and
-		the button sensors.*/
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
-		serialGetchar(kobuki);
-		/*Read the byte containing the button data.*/
-		button = serialGetchar(kobuki);
-
-
-		if (button == 2)
+	while (true)
+	{
+		// If the bytes are a 1 followed by 15, then we are
+		// parsing the basic sensor data packet
+		read = serialGetchar(kobuki);
+		if (read == 1)
 		{
-			cout<<"button B1 pushed"<<endl;
-			serialClose(kobuki);
-			// break;
+			if (serialGetchar(kobuki) == 15)
+				break;
 		}
+	}
 
-		/*Pause the script so the data read receive rate is
-		the same as the Kobuki send rate.*/
-		usleep(20000);
-		
+	// Read past the timestamp
+	serialGetchar(kobuki);
+	serialGetchar(kobuki);
+	/*Read the bytes containing the bumper, wheel drop,
+		and cliff sensors.*/
+	bumper = serialGetchar(kobuki);
+	drop = serialGetchar(kobuki);
+	cliff = serialGetchar(kobuki);
+
+	/*Read through the bytes between the cliff sensors and
+	the button sensors.*/
+	serialGetchar(kobuki);
+	serialGetchar(kobuki);
+	serialGetchar(kobuki);
+	serialGetchar(kobuki);
+	serialGetchar(kobuki);
+	serialGetchar(kobuki);
+	/*Read the byte containing the button data.*/
+	button = serialGetchar(kobuki);
+
+	if (button == 2)
+	{
+		cout << "button B1 pushed" << endl;
+		serialClose(kobuki);
+		// break;
+	}
+
+	/*Pause the script so the data read receive rate is
+	the same as the Kobuki send rate.*/
+	usleep(20000);
 }
